e8bf2bafa0530ab15f3676f4a202c3b7a8c19280
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-02-07 23:06:02 +0100

    fully responsive

---
+++ public/script.js
@@ -0,0 +1,8 @@
+var cards = document.querySelectorAll('.main__card--positioning');
+
+cards.forEach((card) => {
+  card.addEventListener('click', function() {
+    console.log(card)
+    card.classList.toggle('main__card--flip');
+  });
+});
